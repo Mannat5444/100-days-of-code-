ba29999886aa74d6791d7bd6bf1319c80aec1510
Mannat5444 <Mannat.28598@stu.upes.ac.in>
2025-11-29 21:48:19 +0530

    Create day46_q1.c

---
+++ day46_q1.c
@@ -0,0 +1,25 @@
+//Remove all vowels from a string//
+#include <stdio.h>
+
+int main() {
+    char str[200], result[200];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]%*c", str);  
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if (!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+              ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')) {
+            result[j++] = ch;   
+        }
+    }
+
+    result[j] = '\0';  
+
+    printf("String after removing vowels: %s\n", result);
+
+    return 0;
+}
