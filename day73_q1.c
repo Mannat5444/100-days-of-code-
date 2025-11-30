bde0a8c10610035d3114f8e7b231f5f81e6bdffe
Mannat5444 <Mannat.28598@stu.upes.ac.in>
2025-11-30 22:55:55 +0530

    Create day73_q1.c

---
+++ day73_q1.c
@@ -0,0 +1,47 @@
+//Read a text file and count the total number of characters, words, and lines. A word is defined as a sequence of non-space characters separated by spaces or newlines//
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    fp = fopen("info.txt", "r");
+
+    if (fp == NULL) {
+        printf("Error! File not found.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        // Count lines
+        if (ch == '\n')
+            lines++;
+
+        // Check for word boundaries
+        if (isspace(ch)) {
+            if (inWord) {
+                words++;
+                inWord = 0;
+            }
+        } else {
+            inWord = 1;
+        }
+    }
+
+    // Count last word if file does not end with space
+    if (inWord)
+        words++;
+
+    fclose(fp);
+
+    printf("Total Characters: %d\n", characters);
+    printf("Total Words: %d\n", words);
+    printf("Total Lines: %d\n", lines);
+
+    return 0;
+}
