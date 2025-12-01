968177ab88ebaa8aabbf9d892075d65a9ffbad9c
Mannat5444 <Mannat.28598@stu.upes.ac.in>
2025-12-01 11:52:46 +0530

    Create day92_q1.c

---
+++ day92_q1.c
@@ -0,0 +1,35 @@
+//Store details of 5 students in an array of structures and print all//
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student students[5];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+
+    printf("\n--- Student Details ---\n");
+    for (i = 0; i < 5; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Roll Number: %d\n", students[i].roll_no);
+        printf("Marks: %.2f\n", students[i].marks);
+    }
+
+    return 0;
+}
