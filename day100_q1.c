a8c5a96d193657829b9336f554c5670fee1d410e
Mannat5444 <Mannat.28598@stu.upes.ac.in>
2025-12-01 11:57:15 +0530

    Create day100_q1.c

---
+++ day100_q1.c
@@ -0,0 +1,32 @@
+//use pointer to struct to modify and display data using -> operator//
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr;
+
+    ptr = &s;
+
+    printf("Enter name: ");
+    scanf("%s", ptr->name);  
+
+    printf("Enter roll number: ");
+    scanf("%d", &ptr->roll_no);  
+
+    printf("Enter marks: ");
+    scanf("%f", &ptr->marks);  
+
+    printf("\n--- Student Details ---\n");
+    printf("Name: %s\n", ptr->name);
+    printf("Roll Number: %d\n", ptr->roll_no);
+    printf("Marks: %.2f\n", ptr->marks);
+
+    return 0;
+}
